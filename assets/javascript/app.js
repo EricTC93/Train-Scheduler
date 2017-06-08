17fcc08c7b936ce66e4743fa6d68d85dc596113a
YETC7 <tyler_726@live.com>
2017-06-07 22:04:33 -0700

    add click event for submit

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var config = {
+    apiKey: "AIzaSyDPAkRq2y-F_EbAhKtvraZ64gzrzRczBcg",
+    authDomain: "train-scheduler-firebase.firebaseapp.com",
+    databaseURL: "https://train-scheduler-firebase.firebaseio.com",
+    projectId: "train-scheduler-firebase",
+    storageBucket: "train-scheduler-firebase.appspot.com",
+    messagingSenderId: "106759238231"
+  };
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "Test";
+var destination = "test";
+var firstTrain = "00:00";
+var frequency = 10;
+
+var trainList = [];
+
+// var ref = new Firebase(URL_TO_DATA);
+// var newChildRef = ref.push();
+// newChildRef.set({
+// 	trainName: trainName
+// });
+
+$("#submit").on("click",function() {
+	event.preventDefault();
+
+	trainList.push({
+		name: $("#trainName").text(),
+		destination: $("#destination").text(),
+		firstTrain: $("#firstTrain").text(),
+		frequency: $("#frequency").text()
+	});
+
+	console.log(trainList);
+});
