f3f1d89d9dd6377c61a232b7e78949d6ecf9c3f7
YETC7 <tyler_726@live.com>
2017-06-10 01:55:33 -0700

    merge with moment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,32 +12,16 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // Declaring Variables
-var trainList = [{
-	name: "First Train",
-	destination: "home",
-	firstTrain: { hours: 12, minutes: 0 },
-	frequency: 7
-}];
-
 var date = new Date();
 var currentHr = date.getHours();
 var currentMin = date.getMinutes();
 
+var startDay = "9";
+var startMonth = "June";
+var startYear = "2017";
+
 // Runs when a value has been changed
 database.ref().on("value",function(snap) {
-
-	if (snap.child("trainStorage").exists()) {
-		console.log(snap.val().trainStorage);
-		trainList = snap.val().trainStorage;
-		displayTrains();
-	}
-
-	else {
-
-		database.ref().set({
-			trainStorage: trainList
-		});
-	}
 
 }, function(errorObject) {
 	console.log("There was an error: " + errorObject.code);
@@ -50,135 +34,87 @@ $("#submit").on("click",function(event) {
 	// Rejects submit if a form is left blank
 	if ($("#trainName").val() === "" || 
 		$("#destination").val() === "" || 
-		$("#hours").val() === "" || 
-		$("#minutes").val() === "" || 
+		$("#firstTrainHours").val() === "" || 
+		$("#firstTrainMinutes").val() === "" || 
 		$("#frequency").val() === "" ) {
 
 		alert("All fields are required");
 		return;
 	}
 
-	// var trainStartHr = parseInt($("#firstTrainHours").val().trim());
-	// var trainStartMin = parseInt($("#firstTrainMinutes").val().trim());
+	var firstTrainHr = parseInt($("#firstTrainHours").val().trim());
+	var firstTrainMin = parseInt($("#firstTrainMinutes").val().trim());
+	var trainFreq = parseInt($("#frequency").val().trim());
 
-	trainList.push({
+	var startTimeString = firstTrainHr + ":" 
+	+ firstTrainMin;
+
+	if (moment(startTimeString,"HH:mm").isValid() === false) {
+		alert("Start time is invalid");
+		return;
+	}
+
+	if (trainFreq <= 0 || Number.isInteger(trainFreq) === false) {
+		alert("Frequency is invalid");
+		return;
+	}
+
+	database.ref().push({
 		name: $("#trainName").val().trim(),
 		destination: $("#destination").val().trim(),
 		firstTrain: { 
-			hours: parseInt($("#firstTrainHours").val().trim()), 
-			minutes: parseInt($("#firstTrainMinutes").val().trim())
+			hours: firstTrainHr, 
+			minutes: firstTrainMin
 		},
-		frequency: parseInt($("#frequency").val().trim())
+		frequency: trainFreq
 	});
 
-	console.log(trainList);
-
-	database.ref().set({
-		trainStorage: trainList
-	});
-
-	displayTrains();
+	$("#trainName").val("");  
+	$("#destination").val("");  
+	$("#firstTrainHours").val("");  
+	$("#firstTrainMinutes").val("");  
+	$("#frequency").val(""); 
 
 });
 
 // Displays all the trains in the train array
-function displayTrains() {
-	// Empties current table
-	$("#trainTable").empty();
+database.ref().on("child_added", function(childSnapshot) {
 
-	// Creates table header
-	var tableHead = $("<tr>");
-	tableHead.append($("<th>").html("Train Name"))
-		.append($("<th>").html("Destination"))
-		.append($("<th>").html("Frequency (min)"))
-		.append($("<th>").html("Next Arrival"))
-		.append($("<th>").html("Minutes Away"));
-	$("#trainTable").append(tableHead);
+	var minAway = minutesAway(childSnapshot.val().firstTrain.hours,
+			childSnapshot.val().firstTrain.minutes,
+			childSnapshot.val().frequency);
 
-	// Appends table data for each train
-	for (var i = 0; i < trainList.length; i++) {
+	var nextArivl = arrivalTime(minAway);
 
-		var minAway = minutesAway(trainList[i].firstTrain.hours,
-				trainList[i].firstTrain.minutes,
-				trainList[i].frequency);
-
-		var nextArivl = arrivalTime(minAway);
-
-		if (minAway === 0) {
-			minAway = "Now";
-		}
-
-		var newTableRow = $("<tr>");
-		newTableRow.append($("<td>").html(trainList[i].name))
-			.append($("<td>").html(trainList[i].destination))
-			.append($("<td>").html(trainList[i].frequency))
-			.append($("<td>").html(nextArivl))
-			.append($("<td>").html(minAway));
-		$("#trainTable").append(newTableRow);
-	}
-}
-
-// Uses hours and minutes to display in 12hr clock format
-function displayTime(hr,min) {
-	var hrDisplay = parseInt(hr);
-	var minDisplay = parseInt(min);
-	var period = "AM";
-
-	while (minDisplay >= 60) {
-		minDisplay-=60;
-		hrDisplay++;
+	if (minAway === 0) {
+		minAway = "Now";
 	}
 
-	if (hrDisplay > 24 || hrDisplay < 0 ) {
-		hrDisplay = hrDisplay%24;
-	}
+	var newTableRow = $("<tr>");
+	newTableRow.append($("<td>").html(childSnapshot.val().name))
+		.append($("<td>").html(childSnapshot.val().destination))
+		.append($("<td>").html(childSnapshot.val().frequency))
+		.append($("<td>").html(nextArivl))
+		.append($("<td>").html(minAway));
+	$("#trainTable").append(newTableRow);
 
-	if (hrDisplay === 0 || hrDisplay === 24) {
-		hrDisplay = 12;
-	}
-
-	else if (hrDisplay === 12) {
-		period = "PM";
-	}
-
-	else if (hrDisplay > 12) {
-		hrDisplay-=12;
-		period = "PM";
-	}
-
-	if (minDisplay < 10) {
-		minDisplay = "0" + minDisplay;
-	}
-
-	return hrDisplay + ":" + minDisplay + " " + period;
-}
+});
 
 // Calculates the minutes away from the next train
-// Note: If train start time is after the current time
-// it will treat it a if the train started the day before
+// Note: Train starts running on a seperate date in the past
 function minutesAway(hr,min,freq) {
 
-	var startTimeMin = hr*60 + min;
-	var currentTimeMin = currentHr*60 + currentMin;
+	var startTime = moment().year(startYear)
+		.month(startMonth)
+		.date(startDay)
+		.hour(hr)
+		.minutes(min);
+	var currentTime = moment();
+	var diff = currentTime.diff(startTime,"minutes");
 
-	var newMin;
+	var remainder = diff%freq;
 
-	if (startTimeMin-currentTimeMin > freq) {
-		startTimeMin = startTimeMin - 1440;
-	}
-
-	if (startTimeMin-currentTimeMin <= freq && startTimeMin-currentTimeMin >= 0) {
-		return (startTimeMin-currentTimeMin);
-	}
-
-	else if (startTimeMin-currentTimeMin < 0) {
-		newMin = min + freq;
-		return minutesAway(hr,newMin,freq);
-	}
-
-	else {
-		return (startTimeMin-currentTimeMin);
-	}
+	return (freq - remainder);
 }
 
 // Displays the arival time of the train based on the min away from the current time
@@ -186,5 +122,5 @@ function arrivalTime(min) {
 	var hrDisplay = currentHr;
 	var minDisplay = currentMin + min;
 
-	return displayTime(hrDisplay,minDisplay); 
+	return moment().add(min,"minutes").format("hh:mm A");
 }
